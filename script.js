7a80db9e0bc87f864d07cb44d49b2dc27219ea4c
ElBenjas4000 <sbmartingonzalez@escuelasproa.edu.ar>
2025-05-08 19:54:48 -0300

    benja

--- script.js
+++ script.js
@@ -1,7 +1,73 @@
 let imagenes = [
-
+   {
+        "url": "img/bmw_serie_1.jpg",
+        "nombre": "BMW Serie 1",
+        "descripcion": "El BMW Serie 1 es un compacto premium de la marca BMW, reconocido por su dinámica de conducción, diseño deportivo y tecnología innovadora. Se ofrece en versiones de 3 y 5 puertas, con una amplia gama de motores de gasolina y diésel."
+    },
     {
-        "url": "img/"
+        "url": "img/bmw_x3.jpg",
+        "nombre": "BMW X3", 
+        "descripcion": "El BMW X3 es un SUV compacto de lujo, producido por BMW desde 2003. Es conocido por su manejo deportivo, su interior lujoso y su diseño distintivo. Se ofrece en diversas variantes de motor, incluyendo gasolina y diésel, y en versiones con tracción trasera o total."
+    },
+    {
+        "url": "img/bmw_xm.webp",
+        "nombre": "BMW XM", 
+        "descripcion": "El BMW XM es un SUV de lujo híbrido enchufable fabricado por BMW. Se caracteriza por su potente motor V8 TwinPower Turbo de 4,4 litros, combinado con un motor eléctrico, para ofrecer una experiencia de conducción de alto rendimiento."
+    },
+    {
+        "url": "img/bmw_z4.webp",
+        "nombre": "BMW Z4", 
+        "descripcion": "El BMW Z4 es un roadster deportivo de dos plazas, conocido por su diseño elegante y su potente rendimiento. Se caracteriza por su tracción trasera, transmisión automática de ocho velocidades o manual de seis, y diferentes opciones de motor de gasolina."
+    }
+]
+
+
+let atras = document.getElementById('atras');
+let adelante = document.getElementById('adelante');
+let imagen = document.getElementById('img');
+let puntos = document.getElementById('puntos');
+let texto = document.getElementById('texto')
+let actual = 0
+posicionCarrusel()
+
+atras.addEventListener('click', function(){
+    actual -=1
+
+    if (actual == -1){
+        actual = imagenes.length - 1
     }
 
-]
+    imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
+    texto.innerHTML = `
+    <h3>${imagenes[actual].nombre}</h3>
+    <p>${imagenes[actual].descripcion}</p>
+    `
+    posicionCarrusel()
+})  
+adelante.addEventListener('click', function(){
+    actual +=1
+
+    if (actual == imagenes.length){
+        actual = 0
+    }
+
+    imagen.innerHTML = ` <img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img>`
+    texto.innerHTML = `
+    <h3>${imagenes[actual].nombre}</h3>
+    <p>${imagenes[actual].descripcion}</p>
+    `
+    posicionCarrusel()
+})  
+
+function posicionCarrusel() {
+    puntos.innerHTML = ""
+    for (var i = 0; i <imagenes.length; i++){
+        if(i == actual){
+            puntos.innerHTML += '<p class="bold">.<p>'
+        }
+        else{
+            puntos.innerHTML += '<p>.<p>'
+        }
+    } 
+}
+
